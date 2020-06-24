8522d2a8d5463701ae30c557185f9d62a7fa6319
Ronit Dhingra <rdmusic91@Ronits-Mac.fios-router.home>
2020-06-24 12:44:10 -0400

    Modified the .gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift instead for users to implement their own API keys and data info.

--- youtube-onedaybuilds/ConstantsTemplate.swift
+++ youtube-onedaybuilds/ConstantsTemplate.swift
@@ -1,16 +1,24 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-onedaybuilds
 //
-//  Created by Ronit Dhingra on 5/26/20.
+//  Created by Ronit Dhingra on 6/24/20.
 //  Copyright © 2020 Ronit Dhingra. All rights reserved.
 //
 
+/*
+Feel free to uncomment the code below and fill in the given blanks.
+*/
+
 import Foundation
 
+/*
 struct Constants {
     
-    static var API_KEY = "AIzaSyBSZuCx1-nKl9dmLyjpydjVcTsh3GaHGZY"
-    static var PLAYLIST_ID = "UUUgQaAXJYI45hV_V5dSJpoA"
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
+
+*/
+
