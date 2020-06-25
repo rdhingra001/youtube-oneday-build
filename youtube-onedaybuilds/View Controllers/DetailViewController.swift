0adba9df9c5cf16dbe7f8c54e613afcf9ba270cc
Ronit Dhingra <rdmusic91@Ronits-Mac.local>
2020-06-25 18:22:48 -0400

    added the final touches to the project and finalized the details. Added the thumbnail and title and details, as well as implementing the full video when the thumbnail is clicked.

---
+++ youtube-onedaybuilds/View Controllers/DetailViewController.swift
@@ -0,0 +1,77 @@
+//
+//  DetailViewController.swift
+//  youtube-onedaybuilds
+//
+//  Created by Ronit Dhingra on 6/25/20.
+//  Copyright © 2020 Ronit Dhingra. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    var video: Videos?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Clear the fields
+        titleLabel.text = ""
+        dateLabel.text = ""
+        textView.text = ""
+        
+        // Check if there's a video
+        guard video != nil else {
+            return
+        }
+        
+        // Create the embed URL
+        let embedUrlString = Constants.YT_EMBED_URL + video!.videoId
+        
+        // Load it into the webview
+        let url = URL(string: embedUrlString)
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+        // Set the title
+        titleLabel.text = video!.title
+        
+        // Set the date
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        dateLabel.text = df.string(from: video!.published)
+        
+        // Set the description
+        textView.text = video!.description
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
